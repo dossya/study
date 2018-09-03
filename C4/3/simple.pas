f3fe55cf9fd0438eaf94c615f95b809cc28b7341
daria <daiapushkar23@gmail.com>
2018-09-03 22:26:57 +0300

    commit

--- C4/3/simple.pas
+++ C4/3/simple.pas
@@ -7,10 +7,10 @@ var
 begin
   readln(N);
   a := GetMem(N * sizeof(integer));
-  for i := 1 to N do
+  for i := 0 to N - 1 do
     a[i] := random(500) + 1;
-  for i := 1 to N - 1 do
-    for j := i + 1 to N do
+  for i := 0 to N - 2 do
+    for j := i + 1 to N - 1 do
       writeln(a[i], ' ', a[j]);
   FreeMem(a);
   readln();
